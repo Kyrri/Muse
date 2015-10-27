f154760ec5ce1bba25bc8c21859621340c5c578d
Andrew Illmann <aillmann@Andrews-MacBook-Pro.local>
2015-10-27 18:00:00 -0400

    updated sql schema setup script, creates visitor, group, and visitor-group mapping tables

--- sqlscripts/db_setup.sql
+++ sqlscripts/db_setup.sql
@@ -1,5 +1,32 @@
 -- database scripts
 -- create database
+DROP SCHEMA muse_dev;
+
 CREATE SCHEMA `muse_dev` DEFAULT CHARACTER SET utf8 ;
 USE muse_dev ;
 
+-- create tables and sample data
+CREATE TABLE visitor (
+	visitorId INT NOT NULL AUTO_INCREMENT,
+    visitorName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (visitorId)
+);
+
+CREATE TABLE visitorGroup (
+	visitorGroupId INT NOT NULL AUTO_INCREMENT,
+	visitorGroupName VARCHAR(255) NOT NULL,
+	PRIMARY KEY (visitorGroupId)
+);
+
+CREATE TABLE visitorGroupMemeber (
+    visitorGroupId INT NOT NULL,
+    visitorId INT NOT NULL,
+    active TINYINT NOT NULL, 
+    updateTime TIMESTAMP NOT NULL DEFAULT NOW(),
+	PRIMARY KEY (visitorGroupId, visitorId),
+	FOREIGN KEY (visitorGroupId) REFERENCES visitorGroup(visitorGroupId),
+	FOREIGN KEY (visitorId) REFERENCES visitor(visitorId)
+);
+
+INSERT INTO visitor (visitorName)
+VALUES ('Andrew');
