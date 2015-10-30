127d9a9454c91192a859e6d8a1ee2e7e6039ec8d
Lindsay Paterson <lindsaymrjpaterson@gmail.com>
2015-10-30 15:04:07 -0400

    added ajax based query use example

--- nodetest1/app.js
+++ nodetest1/app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var app = require("express")();
 var routes = require('./routes');
-
 // MySQL connection
 var mysql = require('mysql');
 var conn = mysql.createConnection({
@@ -10,15 +9,6 @@ var conn = mysql.createConnection({
   password: '',
   database: 'muse_dev'
 });
-// conn.connect();
-
-// // test query - to sanity check it connects to the right db
-// conn.query('SELECT * FROM visitor LIMIT 1', function(err, results) {
-//   if (err) throw err;
- 
-//   console.log(results[0]);
-// });
-
 
 // Configuration
 
@@ -33,13 +23,32 @@ app.use(express.static(__dirname + '/public'));
   app.get('/artInfo', function(req, res){
      res.render('artInfo', { title: 'Art Info'});
   });
-  app.get('/partial_artInfo0', function(req, res){
-     res.render('partial_artInfo0', { title: 'Art Info', imgref: '/images/cat1.jpg' });
-  });
   //app.get('/artInfo/:id', function(req, res){
     //res.render('artInfo', {title: 'Art Info', data: data, imgref: imgref });
   //});
+app.get('/partial_artInfo0', function(req, res){
+  // conn.connect();
 
+  // // test query - to sanity check it connects to the right db
+  // conn.query('SELECT * FROM visitor LIMIT 1', function(err, results) {
+  //   if (err) throw err;
+   
+  //   console.log(results[0]);
+    //res.render('partial_artInfo0', { title: 'Art Info', imgref: '/images/cat1.jpg', data: results });
+  // });
+  //conn.end();
+
+  res.render('partial_artInfo0', { title: 'Art Info', imgref: '/images/cat1.jpg' });
+     
+  });
+
+// Queries
+
+
+
+
+
+// Run Server
 app.listen(3000, function(){
   //console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
   console.log("Listening on 3000");
