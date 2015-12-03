fe30401a32492dd6a2f58049e2bac040839b2043
Andrew Illmann <aillmann@Andrews-MacBook-Pro.local>
2015-12-02 20:30:13 -0500

    update of db

--- sqldumps/currentdb.sql
+++ sqldumps/currentdb.sql
@@ -16,399 +16,134 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `artifact`
+-- Table structure for table `ageRange`
 --
 
-DROP TABLE IF EXISTS `artifact`;
+DROP TABLE IF EXISTS `ageRange`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `artifact` (
-  `artifactId` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `artifactName` varchar(255) NOT NULL,
-  `displayId` int(11) NOT NULL,
-  PRIMARY KEY (`artifactId`),
-  KEY `displayId` (`displayId`),
-  CONSTRAINT `artifact_ibfk_1` FOREIGN KEY (`displayId`) REFERENCES `display` (`displayId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `ageRange` (
+  `ageRange` int(11) NOT NULL AUTO_INCREMENT,
+  `ageRangeDesc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`ageRange`),
+  UNIQUE KEY `ageRangeDesc` (`ageRangeDesc`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `artifact`
+-- Dumping data for table `ageRange`
 --
 
-LOCK TABLES `artifact` WRITE;
-/*!40000 ALTER TABLE `artifact` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artifact` ENABLE KEYS */;
+LOCK TABLES `ageRange` WRITE;
+/*!40000 ALTER TABLE `ageRange` DISABLE KEYS */;
+INSERT INTO `ageRange` VALUES (1,'18-20'),(2,'20-25'),(3,'25-30'),(4,'30-40'),(5,'40-50'),(6,'50-60'),(7,'60-70'),(8,'70+'),(9,'N/A');
+/*!40000 ALTER TABLE `ageRange` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `artifactTags`
+-- Table structure for table `gender`
 --
 
-DROP TABLE IF EXISTS `artifactTags`;
+DROP TABLE IF EXISTS `gender`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `artifactTags` (
-  `artifactId` int(11) NOT NULL,
-  `tagId` int(11) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `proCreated` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`artifactId`,`tagId`),
-  KEY `tagId` (`tagId`),
-  CONSTRAINT `artifacttags_ibfk_1` FOREIGN KEY (`artifactId`) REFERENCES `artifact` (`artifactId`),
-  CONSTRAINT `artifacttags_ibfk_2` FOREIGN KEY (`tagId`) REFERENCES `tag` (`tagId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `gender` (
+  `gender` int(11) NOT NULL AUTO_INCREMENT,
+  `genderDesc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`gender`),
+  UNIQUE KEY `genderDesc` (`genderDesc`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `artifactTags`
+-- Dumping data for table `gender`
 --
 
-LOCK TABLES `artifactTags` WRITE;
-/*!40000 ALTER TABLE `artifactTags` DISABLE KEYS */;
-/*!40000 ALTER TABLE `artifactTags` ENABLE KEYS */;
+LOCK TABLES `gender` WRITE;
+/*!40000 ALTER TABLE `gender` DISABLE KEYS */;
+INSERT INTO `gender` VALUES (2,'female'),(1,'male'),(3,'N/A');
+/*!40000 ALTER TABLE `gender` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `display`
+-- Table structure for table `login`
 --
 
-DROP TABLE IF EXISTS `display`;
+DROP TABLE IF EXISTS `login`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `display` (
-  `displayId` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `login` (
+  `loginId` int(11) NOT NULL AUTO_INCREMENT,
   `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `displayName` varchar(255) NOT NULL,
-  `exhibitId` int(11) NOT NULL,
-  PRIMARY KEY (`displayId`),
-  KEY `exhibitId` (`exhibitId`),
-  CONSTRAINT `display_ibfk_1` FOREIGN KEY (`exhibitId`) REFERENCES `exhibit` (`exhibitId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `display`
---
-
-LOCK TABLES `display` WRITE;
-/*!40000 ALTER TABLE `display` DISABLE KEYS */;
-/*!40000 ALTER TABLE `display` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `displayTags`
---
-
-DROP TABLE IF EXISTS `displayTags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `displayTags` (
-  `displayId` int(11) NOT NULL,
-  `tagId` int(11) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `proCreated` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`displayId`,`tagId`),
-  KEY `tagId` (`tagId`),
-  CONSTRAINT `displaytags_ibfk_1` FOREIGN KEY (`displayId`) REFERENCES `display` (`displayId`),
-  CONSTRAINT `displaytags_ibfk_2` FOREIGN KEY (`tagId`) REFERENCES `tag` (`tagId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `displayTags`
---
-
-LOCK TABLES `displayTags` WRITE;
-/*!40000 ALTER TABLE `displayTags` DISABLE KEYS */;
-/*!40000 ALTER TABLE `displayTags` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `exhibit`
---
-
-DROP TABLE IF EXISTS `exhibit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `exhibit` (
-  `exhibitId` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `exhibitName` varchar(255) NOT NULL,
-  `museumId` int(11) NOT NULL,
-  PRIMARY KEY (`exhibitId`),
-  KEY `museumId` (`museumId`),
-  CONSTRAINT `exhibit_ibfk_1` FOREIGN KEY (`museumId`) REFERENCES `museum` (`museumId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `exhibit`
---
-
-LOCK TABLES `exhibit` WRITE;
-/*!40000 ALTER TABLE `exhibit` DISABLE KEYS */;
-/*!40000 ALTER TABLE `exhibit` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `exhibitTags`
---
-
-DROP TABLE IF EXISTS `exhibitTags`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `exhibitTags` (
-  `exhibitId` int(11) NOT NULL,
-  `tagId` int(11) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `proCreated` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`exhibitId`,`tagId`),
-  KEY `tagId` (`tagId`),
-  CONSTRAINT `exhibittags_ibfk_1` FOREIGN KEY (`exhibitId`) REFERENCES `exhibit` (`exhibitId`),
-  CONSTRAINT `exhibittsgs_ibfk_2` FOREIGN KEY (`tagId`) REFERENCES `tag` (`tagId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `exhibitTags`
---
-
-LOCK TABLES `exhibitTags` WRITE;
-/*!40000 ALTER TABLE `exhibitTags` DISABLE KEYS */;
-/*!40000 ALTER TABLE `exhibitTags` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `groupMembers`
---
-
-DROP TABLE IF EXISTS `groupMembers`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `groupMembers` (
-  `groupId` int(11) NOT NULL,
+  `loginType` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
   `userId` int(11) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`groupId`,`userId`),
-  KEY `userId` (`userId`),
-  CONSTRAINT `visitorgroupmemeber_ibfk_1` FOREIGN KEY (`groupId`) REFERENCES `groups` (`groupId`),
-  CONSTRAINT `visitorgroupmemeber_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`loginId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `groupMembers`
+-- Dumping data for table `login`
 --
 
-LOCK TABLES `groupMembers` WRITE;
-/*!40000 ALTER TABLE `groupMembers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `groupMembers` ENABLE KEYS */;
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (3,1,1,'test1@test.com','test123',7);
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `groups`
+-- Table structure for table `loginType`
 --
 
-DROP TABLE IF EXISTS `groups`;
+DROP TABLE IF EXISTS `loginType`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `groups` (
-  `groupId` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `groupName` varchar(255) NOT NULL,
-  PRIMARY KEY (`groupId`)
+CREATE TABLE `loginType` (
+  `loginType` int(11) NOT NULL AUTO_INCREMENT,
+  `loginTypeDesc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`loginType`),
+  UNIQUE KEY `loginTypeDesc` (`loginTypeDesc`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `groups`
+-- Dumping data for table `loginType`
 --
 
-LOCK TABLES `groups` WRITE;
-/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+LOCK TABLES `loginType` WRITE;
+/*!40000 ALTER TABLE `loginType` DISABLE KEYS */;
+INSERT INTO `loginType` VALUES (1,'email');
+/*!40000 ALTER TABLE `loginType` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `museum`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `museum`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `museum` (
-  `museumId` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
   `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `museumName` varchar(255) NOT NULL,
-  PRIMARY KEY (`museumId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `firstName` varchar(255) DEFAULT NULL,
+  `lastName` varchar(255) DEFAULT NULL,
+  `gender` int(11) DEFAULT NULL,
+  `ageRange` int(11) DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `museum`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `museum` WRITE;
-/*!40000 ALTER TABLE `museum` DISABLE KEYS */;
-/*!40000 ALTER TABLE `museum` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `plan`
---
-
-DROP TABLE IF EXISTS `plan`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `plan` (
-  `planId` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `proCreated` tinyint(4) DEFAULT NULL,
-  `planTypeId` int(11) NOT NULL,
-  PRIMARY KEY (`planId`),
-  KEY `planTypeId` (`planTypeId`),
-  CONSTRAINT `plan_ibfk_1` FOREIGN KEY (`planTypeId`) REFERENCES `planType` (`planTypeId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `plan`
---
-
-LOCK TABLES `plan` WRITE;
-/*!40000 ALTER TABLE `plan` DISABLE KEYS */;
-/*!40000 ALTER TABLE `plan` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `planArtifacts`
---
-
-DROP TABLE IF EXISTS `planArtifacts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `planArtifacts` (
-  `planId` int(11) NOT NULL,
-  `artifactId` int(11) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`planId`,`artifactId`),
-  KEY `artifactId` (`artifactId`),
-  CONSTRAINT `planartifacts_ibfk_1` FOREIGN KEY (`planId`) REFERENCES `plan` (`planId`),
-  CONSTRAINT `planartifacts_ibfk_2` FOREIGN KEY (`artifactId`) REFERENCES `artifact` (`artifactId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `planArtifacts`
---
-
-LOCK TABLES `planArtifacts` WRITE;
-/*!40000 ALTER TABLE `planArtifacts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `planArtifacts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `planContributors`
---
-
-DROP TABLE IF EXISTS `planContributors`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `planContributors` (
-  `planId` int(11) NOT NULL,
-  `userId` int(11) NOT NULL,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`planId`,`userId`),
-  KEY `userId` (`userId`),
-  CONSTRAINT `plancontributors_ibfk_1` FOREIGN KEY (`planId`) REFERENCES `plan` (`planId`),
-  CONSTRAINT `plancontributors_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `planContributors`
---
-
-LOCK TABLES `planContributors` WRITE;
-/*!40000 ALTER TABLE `planContributors` DISABLE KEYS */;
-/*!40000 ALTER TABLE `planContributors` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `planType`
---
-
-DROP TABLE IF EXISTS `planType`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `planType` (
-  `planTypeId` int(11) NOT NULL AUTO_INCREMENT,
-  `planType` varchar(255) NOT NULL,
-  PRIMARY KEY (`planTypeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `planType`
---
-
-LOCK TABLES `planType` WRITE;
-/*!40000 ALTER TABLE `planType` DISABLE KEYS */;
-INSERT INTO `planType` VALUES (1,'Visitor Defined'),(2,'Curator Defined');
-/*!40000 ALTER TABLE `planType` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tag`
---
-
-DROP TABLE IF EXISTS `tag`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tag` (
-  `tagId` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `tagName` varchar(255) NOT NULL,
-  `proCreated` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`tagId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tag`
---
-
-LOCK TABLES `tag` WRITE;
-/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (7,1,'test1','test11',1,1);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -419,10 +154,11 @@ DROP TABLE IF EXISTS `userType`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userType` (
-  `userTypeId` int(11) NOT NULL AUTO_INCREMENT,
-  `userType` varchar(255) NOT NULL,
-  PRIMARY KEY (`userTypeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `userType` int(11) NOT NULL AUTO_INCREMENT,
+  `userTypeDesc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`userType`),
+  UNIQUE KEY `userTypeDesc` (`userTypeDesc`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -431,70 +167,8 @@ CREATE TABLE `userType` (
 
 LOCK TABLES `userType` WRITE;
 /*!40000 ALTER TABLE `userType` DISABLE KEYS */;
-INSERT INTO `userType` VALUES (1,'Visitor'),(2,'Curator');
+INSERT INTO `userType` VALUES (1,'visitor');
 /*!40000 ALTER TABLE `userType` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `userId` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `userName` varchar(255) NOT NULL,
-  `userType` int(11) NOT NULL,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`userId`),
-  KEY `userType` (`userType`),
-  CONSTRAINT `user_ibfk_1` FOREIGN KEY (`userType`) REFERENCES `userType` (`userTypeId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `visit`
---
-
-DROP TABLE IF EXISTS `visit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `visit` (
-  `visitId` int(11) NOT NULL AUTO_INCREMENT,
-  `active` tinyint(4) NOT NULL DEFAULT '1',
-  `createTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updateTime` timestamp NULL DEFAULT NULL,
-  `groupId` int(11) NOT NULL,
-  `museumId` int(11) NOT NULL,
-  PRIMARY KEY (`visitId`),
-  KEY `groupId` (`groupId`),
-  KEY `museumId` (`museumId`),
-  CONSTRAINT `visit_ibfk_1` FOREIGN KEY (`groupId`) REFERENCES `groups` (`groupId`),
-  CONSTRAINT `visit_ibfk_2` FOREIGN KEY (`museumId`) REFERENCES `museum` (`museumId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `visit`
---
-
-LOCK TABLES `visit` WRITE;
-/*!40000 ALTER TABLE `visit` DISABLE KEYS */;
-/*!40000 ALTER TABLE `visit` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -506,4 +180,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-02 14:52:29
+-- Dump completed on 2015-12-02 20:29:27
