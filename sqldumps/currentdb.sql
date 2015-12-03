e977618bc86a0b29caee94587015fc3d65a839fb
Andrew Illmann <aillmann@Andrews-MacBook-Pro.local>
2015-12-02 21:35:30 -0500

    db update

--- sqldumps/currentdb.sql
+++ sqldumps/currentdb.sql
@@ -16,6 +16,16 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `muse_dev`
+--
+
+/*!40000 DROP DATABASE IF EXISTS `muse_dev`*/;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `muse_dev` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `muse_dev`;
+
+--
 -- Table structure for table `ageRange`
 --
 
@@ -38,6 +48,31 @@ LOCK TABLES `ageRange` WRITE;
 /*!40000 ALTER TABLE `ageRange` DISABLE KEYS */;
 INSERT INTO `ageRange` VALUES (1,'18-20'),(2,'20-25'),(3,'25-30'),(4,'30-40'),(5,'40-50'),(6,'50-60'),(7,'60-70'),(8,'70+'),(9,'N/A');
 /*!40000 ALTER TABLE `ageRange` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `elementType`
+--
+
+DROP TABLE IF EXISTS `elementType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `elementType` (
+  `elementType` int(11) NOT NULL AUTO_INCREMENT,
+  `elementTYpeDesc` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`elementType`),
+  UNIQUE KEY `elementTYpeDesc` (`elementTYpeDesc`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `elementType`
+--
+
+LOCK TABLES `elementType` WRITE;
+/*!40000 ALTER TABLE `elementType` DISABLE KEYS */;
+INSERT INTO `elementType` VALUES (1,'Oil on canvas');
+/*!40000 ALTER TABLE `elementType` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -170,6 +205,100 @@ LOCK TABLES `userType` WRITE;
 INSERT INTO `userType` VALUES (1,'visitor');
 /*!40000 ALTER TABLE `userType` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'muse_dev'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `userCreate` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `userCreate`(
+	IN vlogin VARCHAR(255), 
+    IN vpass VARCHAR(255),
+    IN vloginType INTEGER, 
+    IN vuserType INTEGER,
+    IN vfirstName VARCHAR(255),
+    IN vlastName VARCHAR(255),
+    IN vgender INTEGER,
+    IN vageRange INTEGER,
+    OUT vmsg VARCHAR(255),
+    OUT vsuc TINYINT
+    
+)
+BEGIN
+	-- check if login is valid
+    DECLARE vvalidLogin TINYINT DEFAULT 0;
+    
+	SELECT COUNT(*) INTO vvalidLogin
+    FROM login
+	WHERE login=vlogin AND loginType=vloginType;
+    
+    IF vvalidLogin > 0 THEN
+		SELECT "login already exisits" INTO vmsg;
+        SELECT -1 INTO vsuc;
+	ELSE
+		INSERT INTO user (firstName, lastName, gender, ageRange)
+        VALUES (vfirstName,vlastName,vgender,vageRange);
+        
+        INSERT INTO login (loginType, login, pass, userId)
+        VALUES (vloginType, vlogin, vpass, (SELECT MAX(userId) FROM user));
+        
+        SELECT "user added" INTO vmsg;
+        SELECT 1 INTO vsuc;
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `userLogin` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `userLogin`(
+	IN vlogin VARCHAR(255),
+    IN vpass VARCHAR(255),
+    IN vloginType INTEGER,
+    OUT vuserId INTEGER,
+    OUT vmsg VARCHAR(255)
+)
+BEGIN
+	DECLARE vvalidLogin TINYINT DEFAULT 0;
+    
+	SELECT COUNT(*) INTO vvalidLogin
+    FROM login
+	WHERE login=vlogin AND pass=vpass AND loginType=vloginType;
+    
+    IF vvalidLogin = 0 THEN
+		SELECT "user does not exist or password incorrect" INTO vmsg;
+        SELECT -1 INTO vuserId;
+	ELSE
+		SELECT userId INTO vuserId
+		FROM login
+		WHERE login=vlogin AND pass=vpass AND loginType=vLoginType;
+        
+        SELECT "login sucessful" INTO vmsg;
+    END IF;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -180,4 +309,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-12-02 21:13:29
+-- Dump completed on 2015-12-02 21:35:07
