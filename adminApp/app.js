33da5bd60c6b225dec7a10758406046b649fcf20
Lindsay Paterson <lindsaymrjpaterson@gmail.com>
2016-02-18 19:33:35 -0500

    Example Admin Interface Elements

--- adminApp/app.js
+++ adminApp/app.js
@@ -4,13 +4,13 @@ var app = require("express")();
 var bodyParser = require('body-parser')
 // MySQL connection
 var mysql = require('mysql');
-// var conn = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'root',
-//   database: 'muse_dev',
-//   multipleStatements: true
-// });
+var conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'muse_dev',
+  multipleStatements: true
+});
 
 // Configuration
 app.set('views', __dirname + '/views');
@@ -29,6 +29,21 @@ exports.loggedIn = false;
    
   //  STARTING PAGE  //
     app.get('/:var(|index)?', function(req, res){
+    	 // conn.query('CALL getElementDataFromCode('+req.body.code+',@o1, @o2, @o3,@o4,@o5,@o6,@o7); SELECT @o1, @o2, @o3,@o4,@o5,@o6,@o7', function(err, results) {
+      //     if (err){
+      //       console.log(err);
+      //       //error occured connecting to DB
+      //     }
+      //     else{
+      //       if(results[1][0]['@o1']==-1){
+      //         console.log("Invalid Code");
+      //         //Artwork doesn't exist
+      //       }
+      //       else{
+      //         res.send(true);
+      //       }
+      //     }
+      //   });
       res.render('index', { title: 'Muse Admin'});
     });
 
