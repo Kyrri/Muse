7b064b8f8ee6254705a37721de235cbb04213513
Taylor <taylmart@gmail.com>
2016-02-24 13:09:52 -0500

    Compatability changes to userApp/views

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,23 @@ gulp.task('templates', function () {
 		nfc: true,
 		development: true,
 	};
-
+//Jade transfer
 	gulp.src('userApp/views/*.jade')
 	.pipe(jade({
 		locals: YOUR_LOCALS
 	}))
 	.pipe(gulp.dest('nativeUserApp/www/'));
+
+//CSS transfer
+	gulp.src('userApp/public/stylesheets/*.css')
+	.pipe(gulp.dest('nativeUserApp/www/stylesheets'));
+
+//Images transfer
+	gulp.src('userApp/public/images/*')
+	.pipe(gulp.dest('nativeUserApp/www/images'));
+
+//Javascripts transfer
+	gulp.src(['userApp/public/javascripts/*','!userApp/public/javascripts/NFC.js','!userApp/public/javascripts/nfc_utils.js'])
+	.pipe(gulp.dest('nativeUserApp/www/javascripts'));
 
 });
