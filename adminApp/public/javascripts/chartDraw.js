b301e172544a0e0e0c8c1627d2ce725c599b05b4
Lindsay Paterson <lindsaymrjpaterson@gmail.com>
2016-03-02 22:19:43 -0500

    Admin app basic functionality and tools. ToDo: sync date pickers, filter real data, chartdraw on page init

--- adminApp/public/javascripts/chartDraw.js
+++ adminApp/public/javascripts/chartDraw.js
@@ -1,25 +1,52 @@
-  google.charts.load('current', {packages: ['corechart']});
-  google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {packages: ['corechart']});
 
-  function drawChart(){
-  	var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Weekday');
-      data.addColumn('number', '# Visits');
-      data.addRows([
-        ['Sunday', 20],
-        ['Monday', 10],
-        ['Tuesday', 8],
-        ['Wednesday', 13],
-        ['Thursday', 32],
-        ['Friday', 69],
-        ['Saturday', 83]
-      ]);
+function drawChart(column){
+  var columnName = $('#mytable').find('thead tr td:nth-child('+(column+1)+')').text().slice(0,-2);
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Title');
+    data.addColumn('number', columnName);
+    var totalrows = [];
+    $('tbody', '#mytable').find('tr').each(function(){
+      var row = [];
+      row.push($(this).find('td:nth-child(1)').text());
+      row.push(parseFloat($(this).find('td:nth-child('+(column+1)+')').text()));
+      totalrows.push(row);
+    });
+   data.addRows(totalrows);
     var options = {
-      title: 'Bananas in a Bowl # Visits by Weekday',
+      title: columnName +' for Elements',
       curveType: 'function',
       legend: { position: 'bottom' }
     }
 
-      var chart = new google.visualization.LineChart(document.getElementById('myPieChart'));
+      var chart = new google.visualization.BarChart(document.getElementById('myPieChart'));
       chart.draw(data, options);
   }
+
+
+$(document).ready(function(){
+  $('.datePicker').datepicker();
+  $('#mytable').bind('dynatable:init', function(e, dynatable){
+    dynatable.queries.functions['exhibitName'] = function(record, queryValue) {
+      return (record.exhibitName.toUpperCase().indexOf(queryValue.toUpperCase()) > -1);
+    };
+  }).dynatable({
+    features:{
+      search: false
+    },
+    inputs:{
+      queries: $('#exhibitName')
+    }
+  }).data('dynatable');
+
+  $('#mytable td.metrics').on('click', function(){
+    drawChart($(this).index());
+  });
+  $('.dropdown-menu li a').on('click', function(){
+      var val = $(this).parent().parent().attr('class').split(' ')[1];
+      $('#'+val).text($(this).text());
+  });
+
+
+});
+
