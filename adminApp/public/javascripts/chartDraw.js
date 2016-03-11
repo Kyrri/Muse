41cde6ac3018df037f38a3ca34b35b485e367fd8
Lindsay Paterson <lindsaymrjpaterson@gmail.com>
2016-03-11 17:06:46 -0500

    chartDraw.js commenting

--- adminApp/public/javascripts/chartDraw.js
+++ adminApp/public/javascripts/chartDraw.js
@@ -1,6 +1,8 @@
 google.charts.load('current', {packages: ['corechart']});
 
+//Draw the analytics chart
 function drawChart(column){
+  //Read dynatable, use as chart data
   var columnName = $('#mytable').find('thead tr td:nth-child('+(column+1)+')').text().slice(0,-2);
     var data = new google.visualization.DataTable();
     data.addColumn('string', 'Title');
@@ -13,18 +15,21 @@ function drawChart(column){
       totalrows.push(row);
     });
    data.addRows(totalrows);
+
+   //Set options for chart (styling, column names, title, etc.)
     var options = {
       title: columnName +' for Elements',
       curveType: 'function',
       legend: { position: 'bottom' }
     }
-
+    //Draw the chart
       var chart = new google.visualization.BarChart(document.getElementById('myPieChart'));
       chart.draw(data, options);
   }
 
 
 $(document).ready(function(){
+  //Inject Exhibit Page
     $('.museum').on('click', function(){
         $.ajax('/exhibits',{
           type: "POST",
@@ -44,8 +49,11 @@ $(document).ready(function(){
 
     });
 
+//upddtes DOM elements to incorperate injected page
   function updateExhibitList(){
     var count = 0;
+
+    //Create Date Pickers, set default range from one week ago to current date
     $('.datePicker').each(function(){
       switch(count){
         case 0:
@@ -59,6 +67,8 @@ $(document).ready(function(){
       }
       count++;
     });
+
+    //Initalize dynatable
     $('#mytable').bind('dynatable:init', function(e, dynatable){
       dynatable.queries.functions['exhibitName'] = function(record, queryValue) {
         return (record.exhibitName.toUpperCase().indexOf(queryValue.toUpperCase()) > -1);
@@ -68,18 +78,23 @@ $(document).ready(function(){
         search: false
       },
       inputs:{
+        //Set custom search bar (can be moved wherever we want)
         queries: $('#exhibitName')
       }
     }).data('dynatable');
 
+    //Draw chart when a table column with metrics is clicked
     $('#mytable td.metrics').on('click', function(){
       drawChart($(this).index());
     });
+
+    //Update filters to replace base text with selected text
     $('.dropdown-menu li a').on('click', function(){
         var val = $(this).parent().parent().attr('class').split(' ')[1];
         $('#'+val).text($(this).text());
     });
 
+    //Inject Element list page in place of Exhibit list page
       $('#mytable tbody tr').on('click', function(){
         $.ajax('/pieces',{
           type: "POST",
@@ -99,6 +114,7 @@ $(document).ready(function(){
     });
   }
 
+  //Drills down the breadcrumbs
   function drillDown(){
     var children = $('#drillTrack li').length;
       switch(children){
@@ -115,6 +131,7 @@ $(document).ready(function(){
 
       $('#drillTrack').append('<li class="drill" id="'+nextDrill+'crumb"><a href="#">'+nextDrill+'</a></li>');    
   }
+  //Function for datepicker to get previous week from today
   function getLastWeek(){
     var today = new Date();
     var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
