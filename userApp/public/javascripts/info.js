7bfe79cfd7d20e0436463600adfaf36acc57dd84
Andrew Illmann <aillmann@Andrews-MacBook-Pro.local>
2016-03-14 18:59:53 -0400

    makes changes to record likes and favs

--- userApp/public/javascripts/info.js
+++ userApp/public/javascripts/info.js
@@ -1,11 +1,14 @@
+var isLiked = false;
+var isFav = false;
+
 $(document).ready(function() {
 	$("#likebtn").on('click', function(){
-		var isLiked = this.src.indexOf(window.location.host+'/images/like.png') != -1;
+		isLiked = this.src.indexOf(window.location.host+'/images/like.png') != -1;
     	this.src    = isLiked  ? this.src.replace('/images/like.png', '/images/like_select.png') : this.src.replace('/images/like_select.png','/images/like.png');
 	});
 	$("#favbtn").on('click', function(){
-		var isLiked = this.src.indexOf(window.location.host+'/images/fav.png') != -1;
-    	this.src    = isLiked  ? this.src.replace('/images/fav.png', '/images/fav_select.png') : this.src.replace('/images/fav_select.png','/images/fav.png');
+		isFav = this.src.indexOf(window.location.host+'/images/fav.png') != -1;
+    	this.src    = isFav  ? this.src.replace('/images/fav.png', '/images/fav_select.png') : this.src.replace('/images/fav_select.png','/images/fav.png');
 	});
 
 	$('#nextCheckInButton').on('click', function (e) {
@@ -25,6 +28,42 @@ $(document).ready(function() {
 	        	window.location.replace('/checkIn');
 	    	}
 		});
+		
+		// track likes
+		if(isLiked) {
+			sqlParams = JSON.stringify({
+				'qry' : 3,
+				'params' : { 'interactionTypeId' : 2,
+					'timestamp' : null
+				}
+			});
+			$.ajax('/exec_query', {
+		 		type: "POST",
+		 		contentType: "application/json",
+		        data: sqlParams,								
+		        success: function(result) {
+		        	window.location.replace('/checkIn');
+		    	}
+			});
+		}
+
+		// track favs
+		if(isFav) {
+			sqlParams = JSON.stringify({
+				'qry' : 3,
+				'params' : { 'interactionTypeId' : 3,
+					'timestamp' : null
+				}
+			});
+			$.ajax('/exec_query', {
+		 		type: "POST",
+		 		contentType: "application/json",
+		        data: sqlParams,								
+		        success: function(result) {
+		        	window.location.replace('/checkIn');
+		    	}
+			});
+		}
 	});
 
 	$('#endTourButton').on('click', function (e) {
@@ -59,6 +98,41 @@ $(document).ready(function() {
 	        	window.location.replace('/checkIn');
 	    	}
 		});
+
+		if(isLiked) {
+			sqlParams = JSON.stringify({
+				'qry' : 3,
+				'params' : { 'interactionTypeId' : 2,
+					'timestamp' : null
+				}
+			});
+			$.ajax('/exec_query', {
+		 		type: "POST",
+		 		contentType: "application/json",
+		        data: sqlParams,								
+		        success: function(result) {
+		        	window.location.replace('/checkIn');
+		    	}
+			});
+		}
+
+		// track favs
+		if(isFav) {
+			sqlParams = JSON.stringify({
+				'qry' : 3,
+				'params' : { 'interactionTypeId' : 3,
+					'timestamp' : null
+				}
+			});
+			$.ajax('/exec_query', {
+		 		type: "POST",
+		 		contentType: "application/json",
+		        data: sqlParams,								
+		        success: function(result) {
+		        	window.location.replace('/checkIn');
+		    	}
+			});
+		}
 	});
 
 });
