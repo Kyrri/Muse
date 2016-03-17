cc70f538eb4ae8ec27c9bce2f03cb86d223bba70
Andrew Illmann <aillmann@v1020-wn-191-214.campus-dynamic.uwaterloo.ca>
2016-03-17 18:17:23 -0400

    add folder for data scripting

---
+++ dataScripting/dataScripting.js
@@ -0,0 +1,26 @@
+const environment = 'dev';
+var mysql = require('mysql');
+var squel = require('squel');
+
+var conn;
+switch (environment) {
+  case 'dev' : 
+    conn = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'muse_dev',
+      multipleStatements: true
+    });
+  break;
+  case 'qa' : 
+    conn = mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: 'root',
+      database: 'muse_qa',
+      multipleStatements: true
+    });
+  break;
+}
+console.log('Connected to '+ environment + ' database ...');
